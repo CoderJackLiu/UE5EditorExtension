b992523f50b781d27987511c1b9656499c20f3a8
CoderJackLiu <596770421@qq.com>
2022-10-02 00:16:24 +0800

    +init unreal engine 5.0.3 blank project!

---
+++ Source/UEPluginsDev/UEPluginsDevGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEPluginsDevGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEPLUGINSDEV_API AUEPluginsDevGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
