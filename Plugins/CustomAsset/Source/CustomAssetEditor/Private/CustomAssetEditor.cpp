a98a1bd6f9a1fd9d91abbae33e485ff30b69a3d1
Liuqi <596770421@qq.com>
2023-12-08 18:05:37 +0800

    new class define

---
+++ Plugins/CustomAsset/Source/CustomAssetEditor/Private/CustomAssetEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "CustomAssetEditor.h"
+
+#define LOCTEXT_NAMESPACE "FCustomAssetEditorModule"
+
+void FCustomAssetEditorModule::StartupModule()
+{
+    
+}
+
+void FCustomAssetEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FCustomAssetEditorModule, CustomAssetEditor)
